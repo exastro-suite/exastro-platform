56816710b0d71752d9864347503ecd5c24f20dc0
shiota-2021 <shiota-satoshi@ncontr.com>
2022-02-22 09:41:03 +0900

    update gateway-httpd

--- gateway-httpd/httpd-start.sh
+++ gateway-httpd/httpd-start.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+#   Copyright 2021 NEC Corporation
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
 
 if [ ! -d "/etc/pki/tls/certs/" ]; then
     mkdir -p "/etc/pki/tls/certs/"
@@ -6,7 +19,7 @@ fi
 if [ ! -d "/etc/pki/tls/private/" ]; then
     mkdir -p "/etc/pki/tls/private/"
 fi
-if [ ! -f "/etc/pki/tls/private/exastro-platform-authentication-infra.key" ]; then
+if [ ! -f "/etc/pki/tls/private/exastro-common-auth.key" ]; then
     openssl req \
     -new \
     -x509 \
@@ -14,9 +27,9 @@ if [ ! -f "/etc/pki/tls/private/exastro-platform-authentication-infra.key" ]; th
     -newkey rsa:2048 \
     -days 3650 \
     -nodes \
-    -out /etc/pki/tls/certs/exastro-platform-authentication-infra.pem \
-    -keyout /etc/pki/tls/private/exastro-platform-authentication-infra.key \
-    -subj "/C=JP/ST=Tokyo/CN=gateway-httpd.exastro-platform-authentication-infra.svc"
+    -out /etc/pki/tls/certs/exastro-common-auth.pem \
+    -keyout /etc/pki/tls/private/exastro-common-auth.key \
+    -subj "/C=JP/ST=Tokyo/CN=gateway-httpd.exastro-common-auth.svc"
 fi
 
 httpd -D FOREGROUND
