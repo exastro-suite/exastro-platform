8c086eda8de63b923bd32732791679cc56b323e1
KaNakayama <kazutomo.nakayama@ncontr.com>
2022-07-21 14:31:30 +0900

    add initial db script

---
+++ platform_root/platform_db/initdb/ZZZ_org2.sql
@@ -0,0 +1,40 @@
+-- 仮、本来はOrganization作成時にDBを作成
+CREATE USER org2_user IDENTIFIED BY 'password';
+CREATE DATABASE org2db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+GRANT ALL PRIVILEGES ON org2db.* TO org2_user;
+
+USE org2db;
+
+CREATE TABLE IF NOT EXISTS workspace
+(
+  workspace_id varchar(255) not null,
+  workspace_name varchar(255) not null,
+  create_at datetime not null DEFAULT CURRENT_TIMESTAMP,
+  update_at datetime not null DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (workspace_id)
+);
+
+CREATE TABLE IF NOT EXISTS environment
+(
+  environment_id varchar(255) not null,
+  environment_name varchar(255) not null,
+  create_at datetime not null DEFAULT CURRENT_TIMESTAMP,
+  update_at datetime not null DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (environment_id)
+);
+
+CREATE TABLE IF NOT EXISTS workspace_environment
+(
+  workspace_id varchar(255) not null,
+  environment_id varchar(255) not null,
+  create_at datetime not null DEFAULT CURRENT_TIMESTAMP,
+  update_at datetime not null DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (workspace_id, environment_id)
+);
+
+-- 仮データ
+INSERT INTO platform.organization (organization_id, organization_name) VALUES ("org2", "Eオーガナイゼーション２");
+INSERT INTO platform.organization_db (organization_id, db_host, db_port, db_database, db_user, db_password) VALUES ("org2", "platform-db", 3306, "org2db", "org2_user", "password");
+INSERT INTO org2db.workspace (workspace_id, workspace_name) VALUES ("wsone", "workspace001");
+INSERT INTO org2db.workspace (workspace_id, workspace_name) VALUES ("wstwo", "workspace002");
+INSERT INTO org2db.workspace (workspace_id, workspace_name) VALUES ("wsthree", "workspace003");
