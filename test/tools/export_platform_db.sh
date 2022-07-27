5a893c02072a1abc3428baf80d7b595af45d6305
shiota-2021 <shiota-satoshi@ncontr.com>
2022-07-27 15:29:47 +0900

    platform export tool

---
+++ test/tools/export_platform_db.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+BASENAME=$(basename "$0")
+BASEPATH=$(dirname "$0")
+
+DB_CONTAINER=devcontainer-platform-db-1
+TEMPDIR=/tmp
+DESTPATH=${BASEPATH}/data
+DUMPFILE=${BASENAME}-`date '+%Y%m%d-%H%M'`-$$.dmp
+
+mkdir -p "${DESTPATH}"
+
+DATABASES=$( docker exec -it ${DB_CONTAINER} bash -c \
+            'echo "show databases;" | mysql -u root --password=${MYSQL_ROOT_PASSWORD} | sed -e "1d" -e "/^information_schema\$/d" -e "/^performance_schema\$/d" | tr "\n" " "' )
+
+echo "[INFO] export DATABASES : ${DATABASES}"
+docker exec -it "${DB_CONTAINER}" bash -c "mysqldump -u root --password=\${MYSQL_ROOT_PASSWORD} --result-file ${TEMPDIR}/${DUMPFILE} --add-drop-table --databases ${DATABASES}"
+docker cp ${DB_CONTAINER}:${TEMPDIR}/${DUMPFILE} ${DESTPATH}/${DUMPFILE}
+
+echo "[INFO] ${BASENAME} succeceful : ${DUMPFILE}"
