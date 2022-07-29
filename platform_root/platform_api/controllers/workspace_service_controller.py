87351e4462309baa81db9fd2dc52132d043541ce
KaNakayama <kazutomo.nakayama@ncontr.com>
2022-07-29 19:07:11 +0900

    create role and mapping for workspace create

--- platform_root/platform_api/controllers/workspace_service_controller.py
+++ platform_root/platform_api/controllers/workspace_service_controller.py
@@ -12,11 +12,12 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
+import base64
 import connexion
 from contextlib import closing
 import json
 
-from common_library.common import common
+from common_library.common import common, api_keycloak_call
 from common_library.common.db import DBconnector
 from libs import queries_workspaces
 
@@ -36,19 +37,90 @@ def workspace_create(body, organization_id):
 
     :rtype: Workspace
     """
-    if not connexion.request.is_json:
+    r = connexion.request
+    if not r.is_json:
         raise
 
-    body = connexion.request.get_json()
-    with closing(DBconnector().connect_orgdb(organization_id)) as conn:
-        with conn.cursor() as cursor:
-            parameter = {
-                "workspace_id": body["workspace_id"],
-                "workspace_name": body["workspace_name"],
-                "informations": "{}",
-            }
+    user_id = r.headers.get("User-id")
+    encode_roles = r.headers.get("Roles")
+    roles = base64.b64decode(encode_roles).decode()
+    language = r.headers.get("Language")
 
+    body = r.get_json()
+    workspace_id = body["workspace_id"]
+    workspace_name = body["workspace_name"]
+    environments = body.get("environments") if body.get("environments") else []
+    wsadmin_users = body.get("workspace_administrators") if body.get("workspace_administrators") else []
+    description = body.get("description") if body.get("description") else ""
+
+    db = DBconnector()
+    with closing(db.connect_orgdb(organization_id)) as conn:
+        with conn.cursor() as cursor:
+
+            # DB登録
+            # insert workspace
+            informations = {"environments": environments, "description": description, }
+            parameter = {
+                "workspace_id": workspace_id,
+                "workspace_name": workspace_name,
+                "informations": json.dumps(informations, ensure_ascii=False),
+            }
             cursor.execute(queries_workspaces.SQL_INSERT_WORKSPACE, parameter)
+
+            private = db.get_organization_private(organization_id)
+
+            # サービスアカウントのTOKEN取得
+            # Get a service account token
+            token_response = api_keycloak_call.keycloak_service_account_get_token(
+                organization_id, private.internal_api_client_clientid, private.internal_api_client_secret,
+            )
+            if token_response.status_code != 200:
+                raise common.AuthException(
+                    "client_user_get_token error status:{}, response:{}".format(token_response.status_code, token_response.text)
+                )
+
+            token = json.loads(token_response.text)["access_token"]
+
+            # ws
+            # ロール作成(ws)
+            # create ws role
+            role_name = workspace_id
+            request_response = api_keycloak_call.keycloak_client_role_create(
+                realm_name=organization_id, client_uid=private.internal_api_client_id, role_name=role_name, token=token,
+            )
+
+            # ws-admin
+            # ロール作成(ws-admin)
+            # create ws-admin role
+            role_name = "{}-admin".format(workspace_id)
+            request_response = api_keycloak_call.keycloak_client_role_create(
+                realm_name=organization_id, client_uid=private.user_token_client_id, role_name=role_name, token=token
+            )
+
+            # ws-adminロールにwsロールをcompositeする
+            # ws-admin role composite ws
+            response_get_role = api_keycloak_call.keycloak_client_role_get(
+                realm_name=organization_id, client_id=private.user_token_client_id, role_name=role_name, token=token,
+            )
+            add_roles = [json.loads(response_get_role.text), ]
+            request_response = api_keycloak_call.keycloak_client_role_composites_create(
+                realm_name=organization_id, client_uid=private.user_token_client_id, role_name=role_name, add_roles=add_roles, token=token,
+            )
+
+            # 管理者ユーザーのrole mappingにws-adminロールを追加する
+            # Add ws-admin role to administrat users role mapping
+            wsadmin_users = wsadmin_users + [{"id": user_id}, ]
+            # 重複除去
+            wsadmin_users = [dict(t) for t in {tuple(d.items()) for d in wsadmin_users}]
+            for wsadmin in wsadmin_users:
+                target_user_id = wsadmin.get("id")
+                request_response = api_keycloak_call.keycloak_user_client_role_mapping_create(
+                    realm_name=organization_id, user_id=target_user_id, client_id=private.user_token_client_id, client_roles=add_roles, token=token,
+                )
+
+            # IT Automation call
+            # XXXX
+
             conn.commit()
 
     return common.response_200_ok(body)
--- platform_root/platform_db/initdb/ZZZ_org2.sql
+++ platform_root/platform_db/initdb/ZZZ_org2.sql
@@ -36,9 +36,9 @@ CREATE TABLE IF NOT EXISTS T_WORKSPACE
 --  "USER_TOKEN_CLIENT_CLIENTID","org2",
 --  "USER_TOKEN_CLIENT_ID","org2-ID",
 
---  "INTERNAL_API_CLIENT_CLIENTID","org2-woorkspaces",
---  "INTERNAL_API_CLIENT_ID","woorkspaces-ID",
---  "INTERNAL_API_CLIENT_SECRET","woorkspaces-client-secret",
+--  "INTERNAL_API_CLIENT_CLIENTID","org2-workspaces",
+--  "INTERNAL_API_CLIENT_ID","workspaces-ID",
+--  "INTERNAL_API_CLIENT_SECRET","workspaces-client-secret",
 
 --  "TOKEN_CHECK_CLIENT_CLIENTID","system-org2-auth",
 --  "TOKEN_CHECK_CLIENT_ID","system-auth-ID",
