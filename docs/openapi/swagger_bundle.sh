1297be1dd4a7e30227a206611fd32dc0a7e91301
Eiji-Nakahashi <eiji-nakahashi@ncontr.com>
2022-08-04 01:57:36 +0000

    add referense and organizations end-point

--- docs/openapi/swagger_bundle.sh
+++ docs/openapi/swagger_bundle.sh
@@ -2,3 +2,5 @@
 BASEDIR=$(dirname "$0")
 
 swagger-cli bundle -o ${BASEDIR}/build/openapi.yaml -t yaml ${BASEDIR}/openapi.yaml
+swagger-cli bundle -o ${BASEDIR}/build/user_referense.yaml -t yaml ${BASEDIR}/user_referense.yaml
+swagger-cli bundle -o ${BASEDIR}/build/system_manager_referense.yaml -t yaml ${BASEDIR}/system_manager_referense.yaml
