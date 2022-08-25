ea3087d04288813ee13011abdb5719171d1c8288
Eiji-Nakahashi <eiji-nakahashi@ncontr.com>
2022-08-25 07:31:53 +0000

    filename changed

--- docs/openapi/swagger_bundle.sh
+++ docs/openapi/swagger_bundle.sh
@@ -2,5 +2,5 @@
 BASEDIR=$(dirname "$0")
 
 swagger-cli bundle -o ${BASEDIR}/build/openapi.yaml -t yaml ${BASEDIR}/openapi.yaml
-swagger-cli bundle -o ${BASEDIR}/build/user_referense.yaml -t yaml ${BASEDIR}/user_referense.yaml
-swagger-cli bundle -o ${BASEDIR}/build/system_manager_referense.yaml -t yaml ${BASEDIR}/system_manager_referense.yaml
+swagger-cli bundle -o ${BASEDIR}/build/user_reference.yaml -t yaml ${BASEDIR}/user_reference.yaml
+swagger-cli bundle -o ${BASEDIR}/build/system_manager_reference.yaml -t yaml ${BASEDIR}/system_manager_reference.yaml
