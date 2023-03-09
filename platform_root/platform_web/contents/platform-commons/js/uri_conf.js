72dde7f5e54c3e8a28446f1ec118a4ee2d492ea1
shiota-2021 <shiota-satoshi@ncontr.com>
2023-03-09 15:06:23 +0900

    #731 user edit

--- platform_root/platform_web/contents/platform-commons/js/uri_conf.js
+++ platform_root/platform_web/contents/platform-commons/js/uri_conf.js
@@ -44,6 +44,7 @@ var api_conf = {
             "get": "/api/{organization_id}/platform/users",
             "post": "/api/{organization_id}/platform/users",
             "get": "/api/{organization_id}/platform/users",
+            "getId": "/api/{organization_id}/platform/users/{user_id}",
             "put": "/api/{organization_id}/platform/users/{user_id}",
             "delete": "/api/{organization_id}/platform/users/{user_id}",
         },
