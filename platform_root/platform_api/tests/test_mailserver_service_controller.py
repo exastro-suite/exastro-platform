e0ee18a480c96a11e9374552bf9d6e75e4fc5a80
r-gima <r-gima@ncontr.com>
2023-12-01 11:33:24 +0900

    add test_mailserver_service_controller

---
+++ platform_root/platform_api/tests/test_mailserver_service_controller.py
@@ -0,0 +1,47 @@
+#   Copyright 2023 NEC Corporation
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def test_settings_mailserver_get(connexion_client):
+    """test settings_mailserver_get
+
+    Args:
+        connexion_client (_type_): _description_
+    """
+
+    pass
+
+
+def test_settings_mailserver_create(connexion_client):
+    """test settings_notification_create
+
+    Args:
+        connexion_client (_type_): _description_
+    """
+
+    pass
+
+
+def test_mailserver_delete(connexion_client):
+    """test mailserver_delete
+
+    Args:
+        connexion_client (_type_): _description_
+    """
+
+    pass
