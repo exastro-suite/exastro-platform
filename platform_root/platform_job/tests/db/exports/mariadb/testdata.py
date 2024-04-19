800dac5c582600790daee9628aa5434ab2c0e0cd
KaNakayama <kazutomo.nakayama@ncontr.com>
2024-04-19 16:24:45 +0900

    #1076 platform-job pytest data update

--- platform_root/platform_job/tests/db/exports/mariadb/testdata.py
+++ platform_root/platform_job/tests/db/exports/mariadb/testdata.py
@@ -1,10 +1,10 @@
-ENCRYPT_KEY='GgT7nfJmJgbFAuxesFw5ofmCZ5IBUxos/N8mC9pMFYQ='
+ENCRYPT_KEY='CkrHRGHONk4RNdpdv8yYf1VceRM4OydHcwQSEL3Jwqs='
 KEYCLOAK_DB_NAME='keycloak'
 KEYCLOAK_ADMIN_USER='admin'
 KEYCLOAK_ADMIN_PASSWORD='password'
 ORGANIZATIONS={
     "org1": {
-        "user_id": "2e922cfd-aa4d-45bc-b3d4-569ff9782811",
+        "user_id": "bdd29563-df03-42ff-bd2f-a6d81b11252d",
         "workspace_id": [
             "ws1",
             "ws2",
@@ -12,7 +12,7 @@ ORGANIZATIONS={
         ]
     },
     "org2": {
-        "user_id": "659ea158-11d7-4b87-9b83-dab5031d6d93",
+        "user_id": "69d2a0c0-ffa2-4511-8b31-4a6353e27d93",
         "workspace_id": [
             "ws1",
             "ws2",
@@ -20,7 +20,7 @@ ORGANIZATIONS={
         ]
     },
     "org3": {
-        "user_id": "4150c151-d695-4938-8829-62d4db16df52",
+        "user_id": "e99923b9-19e7-417e-bd97-a0bd3f8aaede",
         "workspace_id": [
             "ws1",
             "ws2",
@@ -28,7 +28,7 @@ ORGANIZATIONS={
         ]
     },
     "org4": {
-        "user_id": "3f94646f-2c12-4603-b953-023de2333b3f",
+        "user_id": "ae654cb4-02e0-41b2-8684-8e25c6f90d8f",
         "workspace_id": [
             "ws1",
             "ws2",
@@ -36,7 +36,7 @@ ORGANIZATIONS={
         ]
     },
     "org5": {
-        "user_id": "985fadcd-bad4-4287-9a7a-d116575291ba",
+        "user_id": "20ffde0d-3644-4204-acb3-03236a2dcc85",
         "workspace_id": [
             "ws1",
             "ws2",
@@ -47,24 +47,24 @@ ORGANIZATIONS={
 DATABASES=[
     "keycloak",
     "platform",
-    "PF_ORG_2L7J3097DNG2SQ3RCOCM",
-    "PF_WS_MMDI0MYD4MGCAF5HB0P5",
-    "PF_WS_NM6D8FF582BEWCMTMIVY",
-    "PF_WS_NA48SOQPMXK72M1DC0FT",
-    "PF_ORG_1YYIH9WO1JHDN5IQJ8JP",
-    "PF_WS_JV3DYXYQL56F2UPXCA29",
-    "PF_WS_HDCXB6M8PVAFMLBI6WR4",
-    "PF_WS_58FLHG0TT9CE4AHZC18Q",
-    "PF_ORG_6R7N845GTLM0CB7RKEFN",
-    "PF_WS_NBHR6VC0W3HFJZ6UPG3C",
-    "PF_WS_RR4LSITKXY9A91Q8XEQ7",
-    "PF_WS_SX6IYYU3I6KV3N6AX2YF",
-    "PF_ORG_FZCX6PPMS0H7FFLKTNXU",
-    "PF_WS_JWBEBVDTKH6XAGDAAT3O",
-    "PF_WS_O422LRLMKIMMGWQESCGB",
-    "PF_WS_8270QAUO5FYK0G0E9CPJ",
-    "PF_ORG_1KLQH91SABBQHLYCB2M5",
-    "PF_WS_W2URAML6BRZSJTKP81Z8",
-    "PF_WS_XEO4JTOEZF9GXNOXTEE8",
-    "PF_WS_V1NE7G0TUX2YNX01EEPP"
+    "PF_ORG_YNES2VQR2I2YUGJF1D4F",
+    "PF_WS_R2919E58AIKEN3FFZIYJ",
+    "PF_WS_HN1RWU9EBUDBWEHLL8XS",
+    "PF_WS_4ZEWFJHAMR5DZBKLACUS",
+    "PF_ORG_800ARPU336CKKPU7M7YN",
+    "PF_WS_8SNMXVR0LXTZ97LOLWDH",
+    "PF_WS_F0E15CBGWVEPGK2J1PF8",
+    "PF_WS_QKSA2VKP8RUBCN715K2W",
+    "PF_ORG_YAR5QY25XD6BXMJ5SFDG",
+    "PF_WS_U8LUQV7XKY1A2TJOQ3SP",
+    "PF_WS_IGSPRCJOWJF2UJAKW6G3",
+    "PF_WS_7YJY2CCFW629AUJ4WTCZ",
+    "PF_ORG_2GAO78AP3Q1YABEMAVJZ",
+    "PF_WS_K4FJN1J126HHD2OMFTCH",
+    "PF_WS_AHU78SD8FMRXBZREAIAD",
+    "PF_WS_QG2K7UAQ4K8RX6P3K48Q",
+    "PF_ORG_IK3PUTG4FDW5XX5UPSAQ",
+    "PF_WS_QT48GKQ1L6K4K6CTJHHW",
+    "PF_WS_I4B7P2QJ308NA64Z3KWC",
+    "PF_WS_27NSYWDS4LDPHVL0QFTA"
 ]
