596750201c63a8618a3d1575343ea1e94d96170f
Eiji-Nakahashi <eiji-nakahashi@ncontr.com>
2025-10-16 15:36:15 +0900

    migration通知イベント追加対応 #2825

--- platform_root/platform_migration/migrations/1_11_0/update_workspace_db.py
+++ platform_root/platform_migration/migrations/1_11_0/update_workspace_db.py
@@ -203,3 +203,11 @@ class update_workspace_db:
                     # 項目追加済みの場合はSkipする
                     globals.logger.info(f"SKIP ALTER TABLE : {sql_alter_table['COLUMN_TO_ADD']['TABLE_NAME']}")
                     self.skip_count += 1
+            
+            # 通知設定の追加(新規イベント（受信時）・新規イベント（統合時）)
+            # Add notification settings (new event (received), new event (consolidated))
+            for sql_update_table in queries_db_workspace.SQL_UPDATE_M_NOTIFICATION_DESTINATION:
+                globals.logger.info(f"SQL EXECUUTE:{sql_update_table}")
+                with conn.cursor() as cur:
+                    cur.execute(sql_update_table)
+                self.ok_count += 1
